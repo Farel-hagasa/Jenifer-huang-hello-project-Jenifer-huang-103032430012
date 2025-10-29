37044ec4af36ae4ebfb42c391a27fc229ece762f
Farel hagasa. <fareltarigan2606@gmail.com.>
2025-10-29 19:30:15 +0700

    first commit

---
+++ KonversiSuhu.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func Reamur(C float64) float64{
+
+	return (4.0/5.0)*C
+}	
+func Fahrenheit(C float64) float64{
+	
+	return  (9.0/5.0 ) * C + 32
+}	
+func Kelvin(C float64) float64{
+	
+	return  (C + 273)
+	
+}
+
+func main() {
+	var awal, akhir , step float64
+	var  C , R , F ,K float64
+
+	fmt.Scan(&awal,&akhir,&step)
+	for C = awal; C<= akhir; C+= step {
+		R = Reamur(C)
+		F = Fahrenheit(C)
+		K = Kelvin(C)
+		fmt.Println("\t\t\t\t\n",C,R,F,K)
+	}	
+		
+}
